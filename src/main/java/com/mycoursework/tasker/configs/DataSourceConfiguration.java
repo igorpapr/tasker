44bc131fe60d05b43ad699dd56658507b4b2bb8a
Ihor Paprotskyi <igorpapr@gmail.com>
2020-05-07 03:41:22 +0300

    Changed datasource

--- src/main/java/com/mycoursework/tasker/configs/DataSourceConfiguration.java
+++ src/main/java/com/mycoursework/tasker/configs/DataSourceConfiguration.java
@@ -8,13 +8,23 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfiguration {
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/taskerdb?&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC&rewriteBatchedStatements=true");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("admin");
+//        return dataSource;
+//    }
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/taskerdb?&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC&rewriteBatchedStatements=true");
-        dataSource.setUsername("root");
-        dataSource.setPassword("admin");
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://ec2-46-137-156-205.eu-west-1.compute.amazonaws.com:5432/dar8qogfcohdrf");
+        dataSource.setUsername("dbyxlbwkgndjez");
+        dataSource.setPassword("df10ad043b3bcd88ae3e21f7754136dff5c1a7c34588acbda9ac307a8fe20cd9");
         return dataSource;
     }
 }
